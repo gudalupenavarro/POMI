b34e7062e10e25cffa9227f5589706a55cf17cd9
gudalupenavarro <guadannavarro04@gmail.com>
2023-07-01 23:20:36 -0300

    Agregando funcionalidad de escribir una tarea y que al enviarla desaparezca del input

--- src/componentes/TareasForm.js
+++ src/componentes/TareasForm.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const TareasForm = () => {
+
+export const TareasForm = ({addTareas}) => {
+    const [value, setValue] = useState("")
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        addTareas(value)
+        setValue("")
+    }
+
+    
   return (
-    <form className='tareasForm'>
-        <input className='tareasInput' type='text' placeholder='Cual es tu tarea hoy?'/>
+    <form className='tareasForm' onSubmit={handleSubmit}>
+        <input className='tareasInput' type='text' placeholder='Cual es tu tarea hoy?'
+         value={value} onChange={(e) => setValue(e.target.value)}/>
         <button type='submit' className='tarea-btn'>Agregar Tarea </button>
     </form>
   )
--- src/componentes/TareasWrapp.js
+++ src/componentes/TareasWrapp.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import TareasForm from './TareasForm'
+import { v4 as uuidv4 } from 'uuid';
+uuidv4();
 
-const TareasWrapp = () => {
+export const TareasWrapp = () => {
+  const [tareas, setTareas] = useState([])
+  const addTareas = tareas =>
+    setTareas([...tareas, {id: uuidv4(), task: tareas,
+       completed: false, isEditing: false}])
+       console.log(tareas)
+
   return (
     <div className='tareasWrapper'>
-        <TareasForm/>
+        <TareasForm addTareas={addTareas}/>
+        
       
     </div>
   )
