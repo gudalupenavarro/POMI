e500722c537c846ee9555ad8d46df1090c1791dd
gudalupenavarro <guadannavarro04@gmail.com>
2023-07-01 22:42:17 -0300

    Agregando tareas wrapp a app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, {useState} from "react";
-
-
-
 import './App.css';
+import TareasWrapp from "./componentes/TareasWrapp";
 
+const App = () => {
+  return (
+    <div className="App">
 
+        <TareasWrapp></TareasWrapp>
+      
+    </div>
+  )
+}
 
-export default App;
+export default App
+
