d1881d8eead8315fdd2159ec68da9c2ab963eec5
gudalupenavarro <guadannavarro04@gmail.com>
2023-07-03 00:35:35 -0300

    Agregando la funcionalidad de que la tarea aparezca abajo, y que al tocarla se tache

--- src/componentes/Tareas.js
+++ src/componentes/Tareas.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-const Tareas = () => {
+
+export const Tareas = ({task, toggleComplete}) => {
+ 
   return (
-    <div>
+    <div className="tarea">
       
+
+      <p onClick={() => toggleComplete(task.id) } className={`${task.completed ? 'completed' : ""}`}>{task.task}</p>   
+       
+        <div>
+          <FontAwesomeIcon className='pen' icon={faPenToSquare} />
+            <FontAwesomeIcon className='trash' icon={faTrash} />
+        </div>
     </div>
   )
 }
 
-export default Tareas
+
