157f618261a18dacde2e6dbaead4986f75138b24
gudalupenavarro <guadannavarro04@gmail.com>
2023-10-22 20:38:57 -0300

    Agregando pantalla

--- src/componentes/TareasForm.js
+++ src/componentes/TareasForm.js
@@ -1,20 +1,21 @@
 import React, {useState} from 'react'
 
 
-export const TareasForm = ({addTareas}) => {
+export const TareasForm = ({addTodo}) => {
     const [value, setValue] = useState("")
 
     const handleSubmit = e => {
         e.preventDefault();
 
-        addTareas(value)
+        addTodo(value);
+
         setValue("")
     }
 
     
   return (
-    <form className='tareasForm' onSubmit={handleSubmit}>
-        <input className='tareasInput' type='text' placeholder='Cual es tu tarea hoy?'
+    <form className='TareasForm' onSubmit={handleSubmit}>
+        <input className='todo-input' type='text' placeholder='Cual es tu tarea hoy?'
          value={value} onChange={(e) => setValue(e.target.value)}/>
         <button type='submit' className='tarea-btn'>Agregar Tarea </button>
     </form>
