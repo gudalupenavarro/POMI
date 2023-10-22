24900135418035f662e26a48980d6dfbfad2cedb
gudalupenavarro <guadannavarro04@gmail.com>
2023-10-22 20:41:17 -0300

    Armando constantes add, edit, delete

--- src/componentes/TareasWrapp.js
+++ src/componentes/TareasWrapp.js
@@ -1,30 +1,56 @@
 import React, {useState} from 'react'
-import { Tareas } from './Tareas';
 import TareasForm from './TareasForm'
 import { v4 as uuidv4 } from 'uuid';
+import { Editar } from './Editar';
+import Tareas, { Todo } from './Tareas';
+
+
 uuidv4();
 
 export const TareasWrapp = () => {
-  const [tareas, setTareas] = useState([])
+  const [todos, setTodos] = useState([])
 
-  const addTareas = tareas =>{ 
-    setTareas([...tareas, {id: uuidv4(), task: tareas,
+  const addTodo = todo =>{ 
+    setTodos([...todos, {id: uuidv4(), task: todo,
        completed: false, isEditing: false}])
-       console.log(tareas)
+       console.log(todos)
       }
 
       const toggleComplete = id => {
-        setTareas(tareas.map(tareas => tareas.id == id ? {
-           ...tareas, completed : !tareas.completed} :tareas))
+        setTodos(todos.map(todo => todo.id == id ? {
+           ...todo, completed : !todo.completed} :todo))
       }
     
+      
+      const deleteTodo = id =>{
+        
+        setTodos(todos.filter(todo=> todo.id !== id))
+      }
+
+      const editTodo = id => {
+      
+      setTodos(todos.map(todo => todo.id === id ? {... todo, isEditing: !todo.isEditing} : todo))
+    }
+
+    const editTask = (task, id) => {
+      setTodos(todos.map(todo => todo.id === id ? {...
+      todo, task, isEditing: !todo.isEditing} : todo))
+    }
+
   return (
-    <div className='tareasWrapper'>
-        <h1>Hace tus tareas!</h1>
-        <TareasForm addTareas={addTareas}/>
-        {tareas.map((tareas, index) => (
-          <Tareas task={tareas} key={index} toggleComplete={toggleComplete} />
-       
+    <div className='TodosWrapper'>
+        <h1>Hace tus todos!</h1>
+        <TareasForm addTodo={addTodo}/>
+        {todos.map((todo, index) => (
+         todo.isEditing ? (
+            <Editar editTodo={editTask} task= {todo}/>
+          ) : (
+          <Todo task={todo} key={index} 
+          toggleComplete={toggleComplete}
+           deleteTodo={deleteTodo}
+           editTodo={editTodo}/>
+           )
+
         ))}
     </div>
   )
